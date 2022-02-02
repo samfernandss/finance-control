b4e80f71bdf17641b9fd11fe833acabbf6013ed3
Sam Fernandes <samwallflowergirl@gmail.com>
2022-02-02 19:08:47 -0300

    state, filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
 
@@ -24,9 +25,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses data={expenses}/>
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2022')
+
+    const filterChangeHandler = (year) => {
+        setFilteredYear(year);
+    };
+
+    console.log(filteredYear);
+
     return (
         <Card className='expenses'>
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
             {props.data.map(expense => {
                 return (<ExpenseItem
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
                 />)
             })}
         </Card>
